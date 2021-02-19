e1b5d491c80ddfd7533974409788d69097c2dea0
Dj_RuBiuS <sarubius@gmail.com>
2021-02-19 12:18:42 +0300

    Initial Commit

---
+++ HW_TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_TrafficLight
+//
+//  Created by Сергей Фролов on 19.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
